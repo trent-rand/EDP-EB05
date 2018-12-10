6729f3e3aed4c561d12c3aae19ba50c7e60f799b
trent-rand <37413469+trent-rand@users.noreply.github.com>
2018-12-10 14:27:31 -0500

    Add files via upload

--- GenSim-Word2Vec.py
+++ GenSim-Word2Vec.py
@@ -3,6 +3,15 @@ import gensim
 import logging
 import os
 
+import numpy as np
+import pandas as pd
+
+import networkx as nx
+
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
+
+
 logging.basicConfig(
     format='%(asctime)s : %(levelname)s : %(message)s',
     level=logging.INFO)
@@ -32,7 +41,7 @@ def read_input(input_file):
 if __name__ == '__main__':
 
     abspath = os.path.dirname(os.path.abspath(__file__))
-    data_file = os.path.join(abspath, "reviews_data.txt.gz")
+    data_file = os.path.join(abspath, "reviews_data_minimus_reduced.txt.gz")
 
     # read the tokenized reviews into a list
     # each review item becomes a serries of words
@@ -50,72 +59,93 @@ if __name__ == '__main__':
     model.train(documents, total_examples=len(documents), epochs=10)
 
     # save only the word vectors
-    model.wv.save(os.path.join(abspath, "../vectors/default"))
+    #model.wv.save("C:/Users/Trent Rand/Documents/EDP-EB05/EDP-EB05-master/vectors/default")
 
-    w1 = "dirty"
-    print("Most similar to {0}".format(w1), model.wv.most_similar(positive=w1))
 
-    # look up top 6 words similar to 'polite'
-    w1 = ["polite"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
+    #vec = get_mean_vector(model, doc.words)
+    print(model.wv);
 
-    # look up top 6 words similar to 'france'
-    w1 = ["france"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
+    word_vectors = model.wv.vocab
 
-    # look up top 6 words similar to 'shocked'
-    w1 = ["shocked"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
+    X = []
+    for word in model.wv.vocab:
+        X.append(model.wv[word])
 
-    # look up top 6 words similar to 'shocked'
-    w1 = ["beautiful"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
+    X = np.array(X)
 
-    # get everything related to stuff on the bed
-    w1 = ["bed", 'sheet', 'pillow']
-    w2 = ['couch']
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            negative=w2,
-            topn=10))
+    print("Computed X: ", X.shape)
 
-    # similarity between two different words
-    print("Similarity between 'dirty' and 'smelly'",
-          model.wv.similarity(w1="dirty", w2="smelly"))
 
-    # similarity between two identical words
-    print("Similarity between 'dirty' and 'dirty'",
-          model.wv.similarity(w1="dirty", w2="dirty"))
+    #my_words = [word for word in my_words if word in model] # filter out words not in model
 
-    # similarity between two unrelated words
-    print("Similarity between 'dirty' and 'clean'",
-          model.wv.similarity(w1="dirty", w2="clean"))
+# The number of connections we want: either as a factor of the number of words or a set number
+    num_top_conns = 150
+
+#######
+
+# Make a list of all word-to-word distances [each as a tuple of (word1,word2,dist)]
+    dists=[]
+
+## Method 1 to find distances: use gensim to get the similarity between each word pair
+    for i1,word1 in enumerate(word_vectors):
+	       for i2,word2 in enumerate(word_vectors):
+		             if i1>=i2: continue
+		             cosine_similarity = model.similarity(word1,word2)
+		             cosine_distance = 1 - cosine_similarity
+		             dist = (word1, word2, cosine_distance)
+		             dists.append(dist)
+
+## Or, Method 2 to find distances: use scipy (faster)
+#from scipy.spatial.distance import pdist,squareform
+#Matrix = np.array([model[word] for word in my_words])
+#dist = squareform(pdist(Matrix,'cosine'))
+#for i1,word1 in enumerate(my_words):
+#$	for i2,word2 in enumerate(my_words):
+#		if i1>=i2: continue
+#		cosine_distance = Matrix[i1, i2]
+#		dist = (word1, word2, cosine_distance)
+#		dists.append(dist)
+
+######
+
+# Sort the list by ascending distance
+    dists.sort(key=lambda _tuple: _tuple[-1])
+
+# Get the top connections
+    top_conns = dists[:num_top_conns]
+
+    g = nx.Graph()
+    #g.relabel_nodes(model.wv[word])
+    for word1,word2,dist in top_conns:
+	       weight = 1 - dist # cosine similarity makes more sense for edge weight
+	       g.add_edge(word1, word2, weight=float(weight))
+
+# Write the network
+    nx.write_graphml(g, 'Word2VecGenSimYelpReviews.graphml')
+
+    nx.draw(g, pos=nx.random_layout(g), with_labels=True, nodecolor='b', edge_color='r')
+    #nx.relabel_nodes(model.wv.vocab)
+    plt.show()
+    #G = nx.Graph()  # build graph
+    #G.add_nodes_from(model.wv)  # add nodes
+    #G.add_edges_from()    #add edges
+
+
+    #X = model[model.wv.vocab]
+    #pca = PCA(n_components=2)
+    #result = pca.fit_transform(X)
+    # create a scatter plot of the projection
+    #pyplot.scatter(result[:, 0], result[:, 1])
+    #words = list(model.wv.vocab)
+    #for i, word in enumerate(words):
+	#       pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+    #pyplot.show()
 
     ######### TEST ##########
     def get_mean_vector(word2vec_model, words):
-    # remove out-of-vocabulary words
-    words = [word for word in words if word in word2vec_model.vocab]
-    if len(words) >= 1:
-        return np.mean(word2vec_model[words], axis=0)
-    else:
-        return []
-    
-     vec = get_mean_vector(model, doc.words)
+        # remove out-of-vocabulary words
+        words = [word for word in words if word in word2vec_model.vocab]
+        if len(words) >= 1:
+            return np.mean(word2vec_model[words], axis=0)
+        else:
+            return []
--- LDA.py
+++ LDA.py
@@ -1,8 +1,13 @@
 ## 3. Import Packages
 import re
+import os
 import numpy as np
 import pandas as pd
 from pprint import pprint
+
+import networkx as nx
+
+import gzip
 
 # Gensim
 import gensim
@@ -26,30 +31,51 @@ import warnings
 warnings.filterwarnings("ignore",category=DeprecationWarning)
 
 
+os.environ['MALLET_HOME'] = 'C:\\users\\biney\\mallet_unzipped\\mallet-2.0.8'
+
+
 ## 5. Prepare Stopwords
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
 
+def read_input(input_file):
+    """This method reads the input file which is in gzip format"""
+
+    logging.info("reading file {0}...this may take a while".format(input_file))
+    with gzip.open(input_file, 'rb') as f:
+        for i, line in enumerate(f):
+
+            if (i % 10000 == 0):
+                logging.info("read {0} reviews".format(i))
+            # do some pre-processing and return list of words for each review
+            # text
+            yield gensim.utils.simple_preprocess(line)
+
 ## 6. Import Newsgroups Data
 # Import Dataset
-df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
-print(df.target_names.unique())
-df.head()
+df = pd.read_csv('C:/Users/Trent Rand/Documents/EDP-EB05/EDP-EB05-master/TrumpTwats.csv', dtype=str)
+#abspath = os.path.dirname(os.path.abspath(__file__))
+#df = os.path.join(abspath, "reviews_data_minimus_reduced.txt.gz")
+
+#print(df.target_names.unique())
+#df.head()
+
+#data = list(read_input(df))
 
 ## 7. Remove emails and newline characters
 # Convert to list
-data = df.content.values.tolist()
+data = df.values.tolist()
 
 # Remove Emails
-data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
+#data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
 
 # Remove new line characters
-data = [re.sub('\s+', ' ', sent) for sent in data]
+#data = [re.sub('\s+', ' ', sent) for sent in data]
 
 # Remove distracting single quotes
-data = [re.sub("\'", "", sent) for sent in data]
+#data = [re.sub("\'", "", sent) for sent in data]
 pprint(data[:1])
 
 
@@ -66,7 +92,7 @@ print(data_words[:1])
 ## 9. Creating Bigram and Trigram Models
 # Build the bigram and trigram models
 bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
-trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
 
 # Faster way to get a sentence clubbed as a trigram/bigram
 bigram_mod = gensim.models.phrases.Phraser(bigram)
@@ -91,10 +117,10 @@ def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
     """https://spacy.io/api/annotation"""
     texts_out = []
     for sent in texts:
-        doc = nlp(" ".join(sent)) 
+        doc = nlp(" ".join(sent))
         texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
     return texts_out
-	
+
 # Remove Stop Words
 data_words_nostops = remove_stopwords(data_words)
 
@@ -108,8 +134,8 @@ nlp = spacy.load('en', disable=['parser', 'ner'])
 # Do lemmatization keeping only noun, adj, vb, adv
 data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
 
-print(data_lemmatized[:1])	
-	
+print(data_lemmatized[:1])
+
 ## 11. Create the Dictionary and Corpus needed for Topic Modeling
 # Create Dictionary
 id2word = corpora.Dictionary(data_lemmatized)
@@ -130,14 +156,14 @@ print(corpus[:1])
 # Build LDA model
 lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                            id2word=id2word,
-                                           num_topics=20, 
+                                           num_topics=50,
                                            random_state=100,
                                            update_every=1,
                                            chunksize=100,
                                            passes=10,
                                            alpha='auto',
                                            per_word_topics=True)
-										  
+
 ## 13. View topics in LDA model
 
 # Print the Keyword in the 10 topics
@@ -150,141 +176,25 @@ doc_lda = lda_model[corpus]
 print('\nPerplexity: ', lda_model.log_perplexity(corpus))  # a measure of how good the model is. lower the better.
 
 # Compute Coherence Score
-coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
-coherence_lda = coherence_model_lda.get_coherence()
-print('\nCoherence Score: ', coherence_lda)
+#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+#coherence_lda = coherence_model_lda.get_coherence()
+#print('\nCoherence Score: ', coherence_lda)
+
+pprint(doc_lda)
+
+
+ndarray = lda_model.get_topics()
+pprint(ndarray)
+
+
+
+nameList = lda_model.get_topic_terms(2)
+pprint(nameList)
+
+
+
+#pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+pyLDAvis.save_html(vis, 'LDATopicModelling(TrumpTweets).html')
 
 ##################################### MALLET ####################################
-
-# Path
-mallet_path = ' C:/Program Files/mallet-2.0.8/bin/mallet' # update this path
-ldamallet = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=20, id2word=id2word)
-
-# Show Topics
-pprint(ldamallet.show_topics(formatted=False))
-
-# Compute Coherence Score
-coherence_model_ldamallet = CoherenceModel(model=ldamallet, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
-coherence_ldamallet = coherence_model_ldamallet.get_coherence()
-print('\nCoherence Score: ', coherence_ldamallet)
-
-## 17. How to find the optimal number of topics for LDA?
-
-def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
-    """
-    Compute c_v coherence for various number of topics
-
-    Parameters:
-    ----------
-    dictionary : Gensim dictionary
-    corpus : Gensim corpus
-    texts : List of input texts
-    limit : Max num of topics
-
-    Returns:
-    -------
-    model_list : List of LDA topic models
-    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
-    """
-    coherence_values = []
-    model_list = []
-    for num_topics in range(start, limit, step):
-        model = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=num_topics, id2word=id2word)
-        model_list.append(model)
-        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
-        coherence_values.append(coherencemodel.get_coherence())
-
-    return model_list, coherence_values
-	
-# Can take a long time to run.
-model_list, coherence_values = compute_coherence_values(dictionary=id2word, corpus=corpus, texts=data_lemmatized, start=2, limit=40, step=6)
-
-# Show graph
-limit=40; start=2; step=6;
-x = range(start, limit, step)
-plt.plot(x, coherence_values)
-plt.xlabel("Num Topics")
-plt.ylabel("Coherence score")
-plt.legend(("coherence_values"), loc='best')
-plt.show()
-
-# Print the coherence scores
-for m, cv in zip(x, coherence_values):
-    print("Num Topics =", m, " has Coherence Value of", round(cv, 4))
-	
-# Select the model and print the topics
-optimal_model = model_list[3]
-model_topics = optimal_model.show_topics(formatted=False)
-pprint(optimal_model.print_topics(num_words=10))
-
-## 18. Finding the dominant topic in each sentence
-def format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data):
-    # Init output
-    sent_topics_df = pd.DataFrame()
-
-    # Get main topic in each document
-    for i, row in enumerate(ldamodel[corpus]):
-        row = sorted(row, key=lambda x: (x[1]), reverse=True)
-        # Get the Dominant topic, Perc Contribution and Keywords for each document
-        for j, (topic_num, prop_topic) in enumerate(row):
-            if j == 0:  # => dominant topic
-                wp = ldamodel.show_topic(topic_num)
-                topic_keywords = ", ".join([word for word, prop in wp])
-                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
-            else:
-                break
-    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
-
-    # Add original text to the end of the output
-    contents = pd.Series(texts)
-    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
-    return(sent_topics_df)
-
-
-df_topic_sents_keywords = format_topics_sentences(ldamodel=optimal_model, corpus=corpus, texts=data)
-
-# Format
-df_dominant_topic = df_topic_sents_keywords.reset_index()
-df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
-
-# Show
-df_dominant_topic.head(10)
-
-## 19. Find the most representative document for each topic
-# Group top 5 sentences under each topic
-sent_topics_sorteddf_mallet = pd.DataFrame()
-
-sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
-
-for i, grp in sent_topics_outdf_grpd:
-    sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet, 
-                                             grp.sort_values(['Perc_Contribution'], ascending=[0]).head(1)], 
-                                            axis=0)
-
-# Reset Index    
-sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
-
-# Format
-sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Text"]
-
-# Show
-sent_topics_sorteddf_mallet.head()
-
-## 20. Topic distribution across documents
-# Number of Documents for Each Topic
-topic_counts = df_topic_sents_keywords['Dominant_Topic'].value_counts()
-
-# Percentage of Documents for Each Topic
-topic_contribution = round(topic_counts/topic_counts.sum(), 4)
-
-# Topic Number and Keywords
-topic_num_keywords = df_topic_sents_keywords[['Dominant_Topic', 'Topic_Keywords']]
-
-# Concatenate Column wise
-df_dominant_topics = pd.concat([topic_num_keywords, topic_counts, topic_contribution], axis=1)
-
-# Change Column names
-df_dominant_topics.columns = ['Dominant_Topic', 'Topic_Keywords', 'Num_Documents', 'Perc_Documents']
-
-# Show
-df_dominant_topics
